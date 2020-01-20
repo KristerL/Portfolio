1a9902d66b8be76a661d8f61143a9055005c7473
Krister Looga <looga.krister@gmail.com>
2020-01-20 13:52:28 +0200

    Updates

---
+++ precache-manifest.4d7dde9eb7ecf6bfb5d458ed28d65a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1143d94c743148ce25cfc8212cf0b313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f6d141881c127213bc",
+    "url": "/static/css/main.9d399826.chunk.css"
+  },
+  {
+    "revision": "8f5a4776f96cb5b9c93f",
+    "url": "/static/js/2.cb4108a7.chunk.js"
+  },
+  {
+    "revision": "a4f6d141881c127213bc",
+    "url": "/static/js/main.6bde0322.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
