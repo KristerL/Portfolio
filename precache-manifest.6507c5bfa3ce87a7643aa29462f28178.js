7dede2027041057e6eeb45801570db79c8cb630f
Krister Looga <looga.krister@gmail.com>
2020-01-20 13:43:51 +0200

    Updates

---
+++ precache-manifest.6507c5bfa3ce87a7643aa29462f28178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23ad368e5951d5c2372f32cb750f5f1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a4f6d141881c127213bc",
+    "url": "/Portfolio/static/css/main.9d399826.chunk.css"
+  },
+  {
+    "revision": "8f5a4776f96cb5b9c93f",
+    "url": "/Portfolio/static/js/2.cb4108a7.chunk.js"
+  },
+  {
+    "revision": "a4f6d141881c127213bc",
+    "url": "/Portfolio/static/js/main.6bde0322.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
